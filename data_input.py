601caae257423de5de17eb0be15ce45aa2e73941
Simone Montali <sim.montali@gmail.com>
2022-03-21 10:36:43 +0100

    Updated padding

--- data_input.py
+++ data_input.py
@@ -9,13 +9,22 @@ class DataInput:
     """
     This class is used to load the data from the data_url and create the train, dev and test datasets.
     """
-    def __init__(self, data_url, train_size, dev_size, dataset_folder="data", split_into_sentences=False, shuffle=False):
+
+    def __init__(
+        self,
+        data_url,
+        train_size,
+        dev_size,
+        dataset_folder="data",
+        split_into_sentences=True,
+        shuffle=False,
+    ):
         """
         Constructor for DataInput class that loads the data from the data_url and creates the train, dev and test datasets.
         The dataset splitted is available through the instance variables self.train, self.dev, self.test
-        
+
         Parameters
-        ----------    
+        ----------
         data_url : URL of the dataset
         train_size : percentage of the dataset that will be used for training
         dev_size : percentage of the dataset that will be used for development
@@ -23,16 +32,23 @@ class DataInput:
         split_into_sentences : boolean indicating if each document in the dataset should be splitted into sentences or not
         shuffle : boolean indicating if the dataset should be shuffled or not before splitting
         """
-        docs = self.import_data(data_url, dataset_folder)           
+        docs = self.import_data(data_url, dataset_folder)
         X, y = self.parse_dataset(docs, split_into_sentences)
-        self.train, self.dev, self.test = self.train_dev_test_split(X, y, train_size, dev_size, path_store=os.path.join(dataset_folder, "split"), shuffle=shuffle)
+        self.train, self.dev, self.test = self.train_dev_test_split(
+            X,
+            y,
+            train_size,
+            dev_size,
+            path_store=os.path.join(dataset_folder, "split"),
+            shuffle=shuffle,
+        )
 
     def import_data(self, data_url, dataset_folder):
         """
         Import dataset from URL.
 
         Parameters
-        ----------  
+        ----------
         data_url : URL of the dataset
         dataset_folder : folder where the dataset will be downloaded
         files_extension : extension of the files inside the dataset (default: dp)
@@ -48,7 +64,7 @@ class DataInput:
         # extract the dataset if it is not extracted
         if not glob.glob(os.path.join(dataset_folder, "**/*.dp"), recursive=True):
 
-            # download the dataset if it does not exist 
+            # download the dataset if it does not exist
             dataset_zip = os.path.join(dataset_folder, data_url.split("/")[-1])
             if not os.path.exists(dataset_zip):
                 print("Downloading the dataset...")
@@ -61,20 +77,25 @@ class DataInput:
                 zip_ref.extractall(dataset_folder)
                 print("Successfully extracted the dataset!")
             os.remove(dataset_zip)
-        
-        dataset_extracted_dir = os.path.join(dataset_folder, os.listdir(dataset_folder)[0])
-        docs = [os.path.join(dataset_extracted_dir, doc) for doc in os.listdir(dataset_extracted_dir)]
+
+        dataset_extracted_dir = os.path.join(
+            dataset_folder, os.listdir(dataset_folder)[0]
+        )
+        docs = [
+            os.path.join(dataset_extracted_dir, doc)
+            for doc in os.listdir(dataset_extracted_dir)
+        ]
         return docs
 
     def parse_dataset(self, docs, split_into_sentences):
         """
         Parse the dependency treebank dataset. This takes into account if the dataset should be splitted into sentences or not (according to self.split_into_sentences).
-        
+
         Parameters
-        ----------  
+        ----------
         docs : list of document paths
         split_into_sentences : boolean indicating if each document in the dataset should be splitted into sentences or not
-        
+
         Returns
         -------
         pair where the first element is a list of lists representing tokens in each document/sentence, the second is a list of lists representing POS tag of tokens in each document/sentence
@@ -84,10 +105,10 @@ class DataInput:
         for doc in docs:
             if split_into_sentences:
                 # if split_into_sentences is True, then we split the document into sentences considering as new sentence all the tokens after an empty line in the document
-                with open(doc, mode='r', encoding='utf-8') as text_file:
+                with open(doc, mode="r", encoding="utf-8") as text_file:
                     sentence_text = []
                     sentence_tags = []
-                    
+
                     # read the doc and extract informations
                     for line in text_file.readlines():
                         if line.strip() == "":
@@ -105,18 +126,14 @@ class DataInput:
                     y.append(np.array(sentence_tags))
             else:
                 # otherwise we consider the whole document as a single data point
-                np_doc = np.loadtxt(doc, str, delimiter="\t", usecols = (0, 1))
+                np_doc = np.loadtxt(doc, str, delimiter="\t", usecols=(0, 1))
                 X.append(np_doc[:, 0])
                 y.append(np_doc[:, 1])
-
-        # add a padding appending dots to the end of each doc to make sure that all docs have the same length
-        max_tokens = max([len(doc) for doc in X])
-        for i, (doc, target) in enumerate(zip(X, y)):
-            X[i] = np.pad(doc, (0, max_tokens-len(doc)), constant_values=".")
-            y[i] = np.pad(target, (0, max_tokens-len(target)), constant_values=".")  
         return np.array(X), np.array(y)
 
-    def train_dev_test_split(self, X, y, train_size, dev_size, path_store=None, shuffle=False):
+    def train_dev_test_split(
+        self, X, y, train_size, dev_size, path_store=None, shuffle=False
+    ):
         """
         Split dataset into train, validation and test.
 
@@ -128,7 +145,7 @@ class DataInput:
         dev_size : percentage of the dataset used for validation (note that test size is 1-train_size-dev_size)
         shuffle : boolean indicating if the dataset should be shuffled before splitting
         path_store : path where the split datasets will be stored. If None, then the split datasets will not be stored.
-        
+
         Returns
         -------
         triple where the first element is the train-set, the second element is the dev-set and the third element is the test-set
@@ -147,26 +164,41 @@ class DataInput:
             os.makedirs(os.path.join(path_store, "test"))
 
         # build the train set
-        train_size = int(np.ceil(train_size*len(X)))
+        train_size = int(np.ceil(train_size * len(X)))
         train_set = (X[:train_size], y[:train_size])
         if path_store is not None:
-            np.savetxt(os.path.join(path_store, "train", "X_train.txt"), train_set[0], fmt="%s")
-            np.savetxt(os.path.join(path_store, "train", "y_train.txt"), train_set[1], fmt="%s")
+            np.savetxt(
+                os.path.join(path_store, "train", "X_train.txt"), train_set[0], fmt="%s"
+            )
+            np.savetxt(
+                os.path.join(path_store, "train", "y_train.txt"), train_set[1], fmt="%s"
+            )
         print("Train set size:", len(train_set[0]))
 
         # build the dev set
-        dev_size = int(np.ceil(dev_size*len(X)))
-        dev_set = (X[train_size:train_size+dev_size], y[train_size:train_size+dev_size])
+        dev_size = int(np.ceil(dev_size * len(X)))
+        dev_set = (
+            X[train_size : train_size + dev_size],
+            y[train_size : train_size + dev_size],
+        )
         if path_store is not None:
-            np.savetxt(os.path.join(path_store, "dev", "X_dev.txt"),  dev_set[0], fmt="%s")
-            np.savetxt(os.path.join(path_store, "dev", "y_dev.txt"),  dev_set[1], fmt="%s")
+            np.savetxt(
+                os.path.join(path_store, "dev", "X_dev.txt"), dev_set[0], fmt="%s"
+            )
+            np.savetxt(
+                os.path.join(path_store, "dev", "y_dev.txt"), dev_set[1], fmt="%s"
+            )
         print("Dev set size:", len(dev_set[0]))
 
         # build the test set
-        test_set = (X[train_size+dev_size:], y[train_size+dev_size:])
+        test_set = (X[train_size + dev_size :], y[train_size + dev_size :])
         if path_store is not None:
-            np.savetxt(os.path.join(path_store, "test", "X_test.txt"), test_set[0], fmt="%s")
-            np.savetxt(os.path.join(path_store, "test", "y_test.txt"), test_set[1], fmt="%s")
+            np.savetxt(
+                os.path.join(path_store, "test", "X_test.txt"), test_set[0], fmt="%s"
+            )
+            np.savetxt(
+                os.path.join(path_store, "test", "y_test.txt"), test_set[1], fmt="%s"
+            )
         print("Test set size:", len(test_set[0]))
         return train_set, dev_set, test_set
 
@@ -182,8 +214,8 @@ class DataInput:
         # lower the tokens in a given set
         if set == "train" or set == "dev" or set == "test":
             for doc in getattr(self, set)[0]:
-                    if to_lower:
-                        for i in range(len(doc)):
-                            doc[i] = doc[i].lower()
+                if to_lower:
+                    for i in range(len(doc)):
+                        doc[i] = doc[i].lower()
         else:
             raise ValueError("Invalid set name. It should be train, dev or test.")
--- text_vectorizer.py
+++ text_vectorizer.py
@@ -17,6 +17,7 @@ class TextVectorizer:
         max_tokens=20000,
         embedding_dim=100,
         embedding_folder="glove",
+        max_size=300,
     ):
         """
         This class parses the GloVe embeddings, the input documents are expected
@@ -32,15 +33,18 @@ class TextVectorizer:
         """
         self.max_tokens = max_tokens
         self.embedding_dim = embedding_dim
-        self.download_glove_if_needed(glove_url=glove_url, embedding_folder=embedding_folder)
-        
+        self.download_glove_if_needed(
+            glove_url=glove_url, embedding_folder=embedding_folder
+        )
+        self.max_size = max_size
+
         # create the vocabulary
-        self.vocabulary = self.parse_glove(embedding_folder) 
+        self.vocabulary = self.parse_glove(embedding_folder)
 
     def download_glove_if_needed(self, glove_url, embedding_folder):
         """
         Downloads the glove embeddings from the internet
-        
+
         Parameters
         ----------
         glove_url : The url of the GloVe embeddings.
@@ -51,9 +55,11 @@ class TextVectorizer:
             os.makedirs(embedding_folder)
 
         # extract the embedding if it is not extracted
-        if not glob.glob(os.path.join(embedding_folder, "**/glove*.txt"), recursive=True):
+        if not glob.glob(
+            os.path.join(embedding_folder, "**/glove*.txt"), recursive=True
+        ):
 
-            # download the embedding if it does not exist 
+            # download the embedding if it does not exist
             embedding_zip = os.path.join(embedding_folder, glove_url.split("/")[-1])
             if not os.path.exists(embedding_zip):
                 print("Downloading the GloVe embeddings...")
@@ -70,17 +76,18 @@ class TextVectorizer:
     def parse_glove(self, embedding_folder):
         """
         Parses the GloVe embeddings from their files, filling the vocabulary.
-        
+
         Parameters
         ----------
         embedding_folder : folder where the embedding files are stored
-        
+
         Returns
         -------
         dictionary representing the vocabulary from the embeddings
         """
-        vocabulary = {}
-        embedding_file = os.path.join(embedding_folder, "glove.6B." + str(self.embedding_dim) + "d.txt")
+        embedding_file = os.path.join(
+            embedding_folder, "glove.6B." + str(self.embedding_dim) + "d.txt"
+        )
         with open(embedding_file, encoding="utf8") as f:
             for line in f:
                 word, coefs = line.split(maxsplit=1)
@@ -91,15 +98,13 @@ class TextVectorizer:
     def adapt(self, documents):
         """
         Computes the OOV words for a single data split, and adds them to the vocabulary.
-        
+
         Parameters
         ----------
         documents : The data split (might be training set, validation set, or test set).
         """
         # create a set containing words from the documents in a given data split
-        words = {
-            word for doc in documents for word in doc
-        }  
+        words = {word for doc in documents for word in doc}
         oov_words = words - self.vocabulary.keys()
 
         # add the OOV words to the vocabulary giving them a random encoding
@@ -113,18 +118,28 @@ class TextVectorizer:
 
         Parameters
         ----------
-        documents : The data split (might be training set, validation set, or test set). 
-        
+        documents : The data split (might be training set, validation set, or test set).
+
         Returns
         -------
         Array of shape (number of documents, number of words, embedding dimension)
         """
-        return np.array([self._transform_document(document) for document in documents])
+        result = []
+        for document in documents:
+            result.append(
+                np.vstack(
+                    (
+                        self._transform_document(document),
+                        np.zeros((self.max_size - len(document), self.embedding_dim)),
+                    )
+                )
+            )
+        return np.array(result)
 
     def _transform_document(self, document):
         """
         Transforms a single document to the GloVe embedding
-        
+
         Parameters
         ----------
         document : The document to be transformed.
@@ -142,12 +157,12 @@ class TextVectorizer:
 
 
 class TargetVectorizer:
-
-    def __init__(self):
+    def __init__(self, max_size=300):
         """
         This class one-hot encodes the target documents, containing the POS tags.
         """
         self.vectorizer = LabelBinarizer()
+        self.max_size = max_size
 
     def adapt(self, targets):
         """
@@ -167,7 +182,7 @@ class TargetVectorizer:
     def transform(self, targets):
         """
         Performs the one-hot encoding for the dataset Ys, returning a list of encoded document tags.
-        
+
         Parameters
         ----------
         targets : The target tags for the dataset split given (it is a list of lists).
@@ -180,7 +195,15 @@ class TargetVectorizer:
             raise NotAdaptedError(
                 "The target vectorizer has not been adapted yet. Please adapt it first."
             )
-        return np.array([self.vectorizer.transform(document) for document in targets])
+
+        result = []
+        for document in targets:
+            transformed = self.vectorizer.transform(document)
+            pad_size = (self.max_size - len(document), transformed.shape[1])
+            result.append(
+                np.vstack((self.vectorizer.transform(document), np.zeros(pad_size)))
+            )
+        return np.array(result)
 
     def inverse_transform(self, targets):
         """
@@ -198,17 +221,20 @@ class TargetVectorizer:
             raise NotAdaptedError(
                 "The target vectorizer has not been adapted yet. Please adapt it first."
             )
-        return np.array([self.vectorizer.inverse_transform(document) for document in targets])
-    
+        return np.array(
+            [self.vectorizer.inverse_transform(document) for document in targets]
+        )
+
     def get_classes(self):
         """
         Returns the classes of the target vectorizer.
-        
+
         Returns
         -------
         List of classes
         """
         return self.vectorizer.classes_
+
 
 if __name__ == "__main__":
     # read data
@@ -232,17 +258,16 @@ if __name__ == "__main__":
 
     # convert inputs to vector representation
     text_vectorizer = TextVectorizer(
-        embedding_dim=50,
-        embedding_folder=os.path.join(os.getcwd(), "embeddings")
+        embedding_dim=50, embedding_folder=os.path.join(os.getcwd(), "embeddings")
     )
     text_vectorizer.adapt(X)
     X = text_vectorizer.transform(X)
 
     # convert targets to one-hot representation
     target_vectorizer = TargetVectorizer()
-    target_vectorizer.adapt(y)  
+    target_vectorizer.adapt(y)
     y = target_vectorizer.transform(y)
-    
+
     print("\nAFTER VECTORIZING")
     print("First input data:")
     print(f"\tShape: {X[0].shape}")
